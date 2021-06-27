6b4de085987a031c96a135a08adf70e09b9e9428
Andy.Wang <andy.wang@clinico.com.tw>
2021-06-27 21:35:08 +0800

    remove env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -13,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to ball's <a href="https://nextjs.org">Next.js!</a>
+          Welcome to balls <Link href="/api/user">user</Link>
         </h1>
 
         <p className={styles.description}>
