5491e9cc12fde98abe46377969a12ccfde7a2862
Andy.Wang <andy.wang@clinico.com.tw>
2021-06-27 20:43:52 +0800

    add: db

---
+++ src/pages/api/user.ts
@@ -0,0 +1,10 @@
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../lib/prisma'
+
+// POST /api/user
+// Required fields in body: name, email
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+  const result = await prisma.user.findMany();
+  res.json(result)
+}
